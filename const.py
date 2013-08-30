25acc01c7363891a4c89491f84efcb9a11852468
Billy J Rhoades II <bjrq48@mst.edu>
2013-08-29 23:39:21 -0500

    Compiles, generates graphs.

--- const.py
+++ const.py
@@ -10,8 +10,17 @@ class gt:
     UNLIT = 1                                 # Unlit tile, default state
     LIT = 2                                   # This is a lit tile, lit by a bulb
     BULB = 3                                  # This is a bulb
+    TRANSFORM = 10                            # What we add to data graph's black value to get our constant
     BLACK0 = 10                               # A black square that cannot be bordered by a light
     BLACK1 = 11                               # A black square that must be bordered by 1 light
     BLACK2 = 12                               # A black square that must be bordered by 2 lights
     BLACK3 = 13                               # A black square that must be bordered by 3 lights
     BLACK4 = 14                               # A black square that must be bordered by 4 lights
+    BLACK = 15                                # A black square that just blocks light
+    
+#Light placement return values
+class lprets:
+    BAD = 0
+    LIT = 1
+    YALIT = 2
+    STOPPED = 3
--- graph.py
+++ graph.py
@@ -2,26 +2,14 @@
 #Author: Billy J Rhoades <bjrq48@mst.edu>
 #Class: CS348 Assignment 1A
 
-from const import gt
+from const import (gt, lprets)
 import fileinput
 
 #Graph class
 class graph:
     def __init__( self, conf ):
         self.data=[]
-        if conf['gen'] != 'True':
-            self.readGraph(conf['gen'])
-        else:
-            self.x = int(conf['x'])
-            self.y = int(conf['y'])
-                
-            #Initilize "blank" graph
-            for i in range(0,(self.x-1)):
-                self.data.append([])
-                for j in range(0,(self.y-1)):
-                    self.data[i].append(gt.UNLIT)
-            self.genGraph(conf)
-                
+
         #Have we made a bad placement?
         self.bad=False
         #Is this an invalid graph?
@@ -33,14 +21,94 @@ class graph:
         #Fitness
         self.fit=-1
 
+        if conf['gen'] != 'True':
+            self.readGraph(conf['gen'])
+        else:
+            self.genGraph(conf)    
+
     def readGraph( self, filename ):
         with fileinput.input(files=(filename)) as fh:
             for line in fh:
-                if fh.filelineno() == 1:
-                    print("hello")
+                ln = fh.filelineno()
+                if ln == 1:
+                    self.x=int(line)
+                if ln == 2:
+                    self.y=int(line)
+                    self.blank()
+                if ln > 2:
+                    rawLine = line.split(' ')
+                    #1 10 5
+                    #(x) (y) (black #)
+                    x = int(rawLine[0])-1             #vvv
+                    y = int(rawLine[1])-1             #Adjust for the sake of arrays
+                    b = int(rawLine[2])
+                    #Skip this line if it's invalid and complain
+                    if x > self.x or y > self.y or x < 0 or y < 0 or b > 5 or b < 0:
+                        print("Line is invalid (", x, ",", y, ") w/ Black of: ", b)
+                        next
+                    self.addLight( x, y, b )
+                    
             fh.close()
         return
 
     def genGraph( self, conf ):
-    
+        self.x = conf['x']
+        self.y = conf['y']
+        self.blank()
         return
+    
+    def blank( self ):        
+        #Initilize "blank" graph
+        for i in range(0,self.x):
+            self.data.append([])
+            for j in range(0,self.y):
+                self.data[i].append(gt.UNLIT)
+        return
+        
+    def addLight( self, x, y, b ):
+        #Add the light to the data list
+        #print( "x:", x, " y:", y, " self.x: ", self.x, " self.y", y )
+        self.data[x][y] = b+gt.TRANSFORM
+        self.lights += 1
+        
+        #Check surrounding spots for validation
+        #FIXME
+        
+        #Light up a line vertically and horizontally
+        self.lightUpPlus( x, y, "x" )
+        self.lightUpPlus( x, y, "y" )
+
+            
+    def lightUpPlus( self, x, y, axis ):
+        #Light up squares on y=$x from (x,self.x]
+        if axis == "x":
+            for i in range(x, 0):
+                if self.lightSq( i, y ) == lprets.STOPPED:
+                    break
+            for i in range(x, self.x):
+                if self.lightSq( i, y ) == lprets.STOPPED:
+                    break
+        else:
+            for i in range(y, 0):
+                if self.lightSq( x, i ) == lprets.STOPPED:
+                    break
+            for i in range(y, self.y):
+                if self.lightSq( x, i ) == lprets.STOPPED:
+                    break
+
+    def lightSq( self, x, y ):
+        #Unlit, light
+        if self.data[x][y] == gt.UNLIT:
+            self.litsq += 1
+            self.data[x][y] = gt.LIT 
+            return lprets.LIT
+        #Do nothing if lit
+        if self.data[x][y] == gt.LIT:
+            return lprets.YALIT
+        #Bulb == invalid
+        if self.data[x][y] == gt.BULB:
+            self.bad= True
+            return lprets.BAD
+        #Black space stopped us
+        if self.data[x][y] == gt.BLACK0:
+            return lprets.STOPPED
