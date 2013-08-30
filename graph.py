8c8e17d497eff3202340aca2d3e7973280f43b22
Billy J Rhoades II <bjrq48@mst.edu>
2013-08-29 19:40:24 -0500

    Basic reading ability

--- graph.py
+++ graph.py
@@ -9,13 +9,18 @@ import fileinput
 class graph:
     def __init__( self, conf ):
         self.data=[]
-        self.x = int(conf['x'])
-        self.y = int(conf['y'])
-        
-        for i in range(0,(self.x-1)):
-            self.data.append([])
-            for j in range(0,(self.y-1)):
-                self.data[i].append(gt.UNLIT)
+        if conf['gen'] != 'True':
+            self.readGraph(conf['gen'])
+        else:
+            self.x = int(conf['x'])
+            self.y = int(conf['y'])
+                
+            #Initilize "blank" graph
+            for i in range(0,(self.x-1)):
+                self.data.append([])
+                for j in range(0,(self.y-1)):
+                    self.data[i].append(gt.UNLIT)
+            self.genGraph(conf)
                 
         #Have we made a bad placement?
         self.bad=False
@@ -25,6 +30,17 @@ class graph:
         self.lights=0
         #How many squares are lit
         self.litsq=0
-
         #Fitness
         self.fit=-1
+
+    def readGraph( self, filename ):
+        with fileinput.input(files=(filename)) as fh:
+            for line in fh:
+                if fh.filelineno() == 1:
+                    print("hello")
+            fh.close()
+        return
+
+    def genGraph( self, conf ):
+    
+        return
