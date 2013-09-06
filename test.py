dbb5e587b28ff0a4452d7bebb33d8aeb7dc6af42
Billy J Rhoades II <bjrq48@mst.edu>
2013-09-05 22:44:28 -0500

    Added testunit files.

---
+++ test.py
@@ -0,0 +1,57 @@
+import os, unittest, profile, timeit, time, datetime
+import sys
+import main
+import graph
+    
+rstd=sys.stdout
+    
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        self.puzzles=[]
+        self.times=[]
+
+    def test_randomGen(self):
+        times = []
+        count = 250
+        size = 10
+        
+        print( ''.join(["Square board generation tests (", str(count)," ea), ignoring black tiles: "]), file=rstd)
+        
+        for size in range(2,size+1):
+            print( ''.join(["  ", str(size), "x", str(size), ": "]), file=rstd, end='')
+              
+            for i in range(0,count):
+                st = datetime.datetime.now()
+                graph.graph( file="cfgs/test-1.cfg", x=size, y=size )
+                t = datetime.datetime.now( ) - st            
+                times.append(t.microseconds)
+            
+            avg = 0
+            for t in times:
+              avg += t
+            avg /= count
+            
+            
+            print( avg, " µs", file=rstd )
+            
+        print( ''.join(["Square board generation tests (", str(count)," ea), counting black tiles: "]), file=rstd)
+        
+        for size in range(2,size+1):
+            print( ''.join(["  ", str(size), "x", str(size), ": "]), file=rstd, end='')
+              
+            for i in range(0,count):
+                st = datetime.datetime.now()
+                graph.graph( file="cfgs/test-2.cfg", x=size, y=size )
+                t = datetime.datetime.now( ) - st            
+                times.append(t.microseconds)
+            
+            avg = 0
+            for t in times:
+              avg += t
+            avg /= count
+            
+            
+            print( avg, " µs", file=rstd )
+         
+if __name__ == '__main__':
+    unittest.main(buffer=True)
